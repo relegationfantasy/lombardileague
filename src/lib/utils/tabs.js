cc4bfcc4695732214d7102f707c8c296cad10a66
relegationfantasy <admin@relegationfantasy.com>
2024-10-28 14:28:53 -0400

    Update tabs.js

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -74,7 +74,7 @@ export const tabs = [
         ]
     },
     {
-        icon: '	fa fa-info-circle',
+        icon: 'fa fa-info-circle',
         label: 'RFL Home',
         dest: 'https://relegationfantasy.com',
     },
